9c371ab2bcd6784e8d0058288fd1a80c74ab5895
newkrux <newkrux@gmail.com>
2024-03-11 15:12:36 -0500

    Update entrypoint.sh
    exec /entrypoint.sh: exec format error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
-
 #!/usr/bin/env bash
 
 set -e
 set -u
 set -o pipefail
-
-#if [ -n "${PARAMETER_STORE:-}" ]; then
-#  export PLANEACION_REPORTES_MID_PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}planeacion_reportes_mid/db/username --output text --query Parameter.Value)"
-#  export PLANEACION_REPORTES_MID_PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/planeacion_reportes_mid/db/password --output text --query Parameter.Value)"
 
 exec ./main "$@"
